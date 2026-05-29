dfd93c29fd02349e50a3973f05374206003c4302
erica00201 <2425403760@live.gctu.edu.gh>
2026-05-29 15:43:06 +0000

    Add files via upload
    2425403760 Erica Mawuena Worchie

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+# File: initials.py
+# Description: Print out my initials .
+# Assignment Number: 2
+#
+# Name: Erica Mawuena Worchie
+# STUDENT ID: 2425403760
+# Email: 2425403760@live.gctu.edu.gh
+# Grader: Mr.Augustus Buckman 
+#
+# On my honor, Erica Mawuena Worchie, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print initials in large form,as 12x10 block letters.
 
+
+    print()
+    print("...EMW")
+    print()
+    print("...EEEEEEEEEEEE........MMMM....MMMM........WW...WW...WW.....")
+    print("...EEEEEEEEEEEE........MMMM....MMMM........WW...WW...WW.....")
+    print("...EEE.................MM.MM..MM.MM........WW...WW...WW.....")
+    print("...EEE.................MM.MM..MM.MM........WW..WWWW..WW.....")
+    print("...EEEEEEEEEEEE........MM.MM..MM.MM........WW..WWWW..WW.....")
+    print("...EEEEEEEEEEEE........MM..MMMM..MM........WW.WW..WW.WW.....")
+    print("...EEE.................MM..MMMM..MM........WW.WW..WW.WW.....")
+    print("...EEE.................MM...MM...MM... ....WW.WW..WW.WW.....")
+    print("...EEEEEEEEEEEE...**...MM...MM...MM...**...WWWW....WWWW...**")
+    print("...EEEEEEEEEEEE...**...MM...MM...MM...**...WWWW....WWWW...**")
+    print()
 
 
 main()
